5f99e87a9992b3b5a1dfbd9c1c7d5d4dcb88a6df
Abdoul Dgit config --global user.email abdoulson02@gmail.com <abdoulson02@gmail.com>
2021-01-26 15:21:11 -0500

    completion of exercices 1 2 and 3

--- app.js
+++ app.js
@@ -4,16 +4,51 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
+let add = a + b; 
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+console.log(a, b, add, minus, multiply, dividing);
+//("EXERCISE 1:\n==========\n");
+
+
+;
 
 // YOUR CODE HERE
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
-
-// YOUR CODE HERE
+ /* a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+console.log(a + str);
+console.log(a + str2);
+console.log(a + isPresent);
+console.log(a + firstName);
+console.log(a + lastName); */
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+a = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+let firstName = "Robin";
+let lastName = "Williams";
+
+console.log(a == str);
+console.log(a === str);
+console.log(isPresent);
+console.log("eleven" == str2 && a >= str);
+console.log(!isPresent || isPresent);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
+
+
 
 // YOUR CODE HERE
